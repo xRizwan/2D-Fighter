524f58f709a97ef0f13adf2cab8380c1fbcb9c54
Rizwan <xrizwanr@gmail.com>
2021-08-24 09:16:15 +0500

    Added menu

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuUI : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("PvP");
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
+    }
+}
