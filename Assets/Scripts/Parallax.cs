d729d342997bfb41d0d60e5d438eb0cc5e8d3cd1
Rizwan <xrizwanr@gmail.com>
2021-09-10 23:33:04 +0500

    removed unncessary comments

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -7,14 +7,14 @@ public class Parallax : MonoBehaviour
     private float length, startpos;
     public GameObject cam;
     public float parallaxEffect;
-    // Start is called before the first frame update
+    
+    
     void Start()
     {
         startpos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
