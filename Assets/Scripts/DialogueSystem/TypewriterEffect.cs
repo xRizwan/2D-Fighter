c36b9c5a0266ec502ac00b69218e1f57c7f0691d
Rizwan <xrizwanr@gmail.com>
2021-09-14 06:18:58 +0500

    created a Punctuation struct

--- Assets/Scripts/DialogueSystem/TypewriterEffect.cs
+++ Assets/Scripts/DialogueSystem/TypewriterEffect.cs
@@ -8,10 +8,10 @@ public class TypewriterEffect : MonoBehaviour
     [SerializeField] private float typewriterSpeed = 50.0f;
     
     public bool IsRunning { get; private set; }
-    private readonly Dictionary<HashSet<char>, float> punctuation = new Dictionary<HashSet<char>, float>()
+    private readonly List<Punctuation> punctuation = new List<Punctuation>()
     {
-        { new HashSet<char>(){'.', '!', '?'}, 0.6f},
-        { new HashSet<char>(){',', ';', ':'}, 0.3f},
+        new Punctuation(new HashSet<char>(){',', ';', ':'}, 0.3f),
+        new Punctuation(new HashSet<char>(){'.', '!', '?'}, 0.6f),
     };
 
     private Coroutine typingCoroutine;
@@ -62,16 +62,28 @@ public class TypewriterEffect : MonoBehaviour
 
     private bool IsPunctuation(char character, out float waitTime)
     {
-        foreach(KeyValuePair<HashSet<char>, float> punctuationCategory in punctuation)
+        foreach(Punctuation punctuationCategory in punctuation)
         {
-            if (punctuationCategory.Key.Contains(character))
+            if (punctuationCategory.Punctuations.Contains(character))
             {
-                waitTime = punctuationCategory.Value;
+                waitTime = punctuationCategory.WaitTime;
                 return true;
             }
         }
 
         waitTime = default;
         return false;
+    }
+
+    private readonly struct Punctuation
+    {
+        public readonly HashSet<char> Punctuations;
+        public readonly float WaitTime;
+
+        public Punctuation(HashSet<char> punctuations, float waitTime)
+        {
+            Punctuations = punctuations;
+            WaitTime = waitTime;
+        }
     }
 }
