bcc1bd7cb2c1f211bd9747e1e17e70e909563129
Rizwan <xrizwanr@gmail.com>
2021-09-17 04:54:58 +0500

    removed unused using statements

--- Assets/Scripts/DialogueSystem/ResponseHandler.cs
+++ Assets/Scripts/DialogueSystem/ResponseHandler.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Events;
-using UnityEngine.EventSystems;
 using TMPro;
 
 public class ResponseHandler : MonoBehaviour
