10e65f7decf8ae7d0745f2e0b155240c5d1a77f6
Rizwan <xrizwanr@gmail.com>
2021-09-17 04:53:58 +0500

    added option to select responses through keyboard along with pressing with mouse (for dialog system)

--- Assets/Scripts/DialogueSystem/ResponseHandler.cs
+++ Assets/Scripts/DialogueSystem/ResponseHandler.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using TMPro;
 
 public class ResponseHandler : MonoBehaviour
@@ -11,6 +13,9 @@ public class ResponseHandler : MonoBehaviour
     [SerializeField] private RectTransform responseContainer;
     private DialogueUI dialogueUI;
     private ResponseEvent[] responseEvents;
+    private int chosenButton;
+    private bool isDisplayingResponses;
+    private Coroutine chosingCoroutine;
 
     private void Start()
     {
@@ -27,6 +32,7 @@ public class ResponseHandler : MonoBehaviour
     public void ShowResponses(Response[] responses)
     {
         // float responseBoxHeight = 0;
+        isDisplayingResponses = true;
 
         for (int i = 0; i < responses.Length; i++)
         {
@@ -44,13 +50,40 @@ public class ResponseHandler : MonoBehaviour
             // responseBoxHeight += responseButtonTemplate.sizeDelta.y;
         }
 
+
         // responseBox.sizeDelta = new Vector2(responseBox.sizeDelta.x, responseBoxHeight);
         responseBox.gameObject.SetActive(true);
+        
+        chosenButton = 0;
+        HandleSelect();
+        chosingCoroutine = StartCoroutine(Chose(responses));
+    }
+
+    IEnumerator Chose(Response[] responses)
+    {
+
+        while (isDisplayingResponses) {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                chosenButton++;
+                HandleSelect();
+            }
+
+            yield return null;
+            if (Input.GetKeyDown(KeyCode.Space)) break;
+        }
+
+        OnPickedResponse(responses[chosenButton], chosenButton);
+
     }
 
     public void OnPickedResponse(Response response, int responseIndex)
     {
+        if (chosingCoroutine != null) StopCoroutine(chosingCoroutine);
+        isDisplayingResponses = false;
         responseBox.gameObject.SetActive(false);
+
+        Debug.Log("here 3 " + "   " + response + "  :  " + responseEvents.Length + "   :  " + responseIndex);
 
         foreach (GameObject button in tempResponseButtons)
         {
@@ -73,4 +106,23 @@ public class ResponseHandler : MonoBehaviour
             dialogueUI.CloseDialogueBox();
         }
     }
+    void SetSelectedColor(TMP_Text text)
+    {
+        text.color = Color.blue;
+    }
+
+    void SetDisabledColor(TMP_Text text)
+    {
+        text.color = Color.black;
+    }
+
+    void HandleSelect()
+    {
+        for(int i = 0; i < tempResponseButtons.Count; i++)
+        {
+            if (i == chosenButton) tempResponseButtons[i].GetComponent<TMP_Text>().color = Color.blue;
+            else tempResponseButtons[i].GetComponent<TMP_Text>().color = Color.black;           
+        }
+    }
 }
+
